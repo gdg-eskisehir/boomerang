646357d42fa4860c170d00fb91b2ff813cad29f4
Oguzhan Uzman <ouzmann@gmail.com>
2016-03-25 00:37:40 +0200

    gdgesk-#5 eski commit duzeltildi biraz. inline css ayni durumda. description hala duzelecek.

--- app/about/about.js
+++ app/about/about.js
@@ -5,11 +5,25 @@ angular.module('gdgXBoomerang')
     NavService.setNavTab(0);
     vm.cover = Config.cover;
 
+    var debugDesc = function () {
+        console.log("asd");
+        console.log(vm.desc);
+    }
+
+    debugDesc();
+
     $http.jsonp('https://www.googleapis.com/plus/v1/people/' + Config.id +
             '?callback=JSON_CALLBACK&fields=aboutMe%2Ccover%2Cimage%2CplusOneCount&key=' + Config.googleApi).
         success(function (data) {
-            vm.desc = data.aboutMe;
+            vm.desc = data.aboutMe
+                    .replace(/<strong>/g, '<strong style=\'font-size: 20pt;\'>')
+                    .replace(/<div>&nbsp;<\/div>/g, '')
+                    .replace(/<strong>&nbsp;<\/strong>/g, '');
+;
+
             $sce.trustAsHtml(vm.desc);
+            debugDesc();
+
 
             if (data.cover && data.cover.coverPhoto.url) {
                 vm.cover.url = data.cover.coverPhoto.url;
